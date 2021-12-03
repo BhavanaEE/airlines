851ff8f6b06f1c06052db5dc699ad966df3520d2
BhavanaEE <bhavana.chivukula@everest.engineering>
2021-12-03 12:52:00 +0530

    Extended search flight service using departure date

--- src/main/java/com/everest/service/SearchFlightService.java
+++ src/main/java/com/everest/service/SearchFlightService.java
@@ -8,10 +8,14 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class SearchFlightService {
-    public List<Flight> getFlights(String source, String destination){
+    public List<Flight> getFlights(String source, String destination,String departureDate){
         List<Flight> flightList = Data.flights.stream()
                 .filter(flight -> flight.getSource().equalsIgnoreCase(source) && flight.getDestination().equalsIgnoreCase(destination))
                 .collect(Collectors.toList());
-        return flightList;
+        return getFlightsByDepartureDate(flightList,departureDate);
     }
+    public List<Flight> getFlightsByDepartureDate(List<Flight> flightList, String departureDate) {
+        return flightList.stream().filter(flight -> flight.getDepartureDate().equals(LocalDate.parse(departureDate))).collect(Collectors.toList());
+    }
+
 }
