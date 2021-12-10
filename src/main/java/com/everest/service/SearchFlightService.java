2dcd6df2901d7c528411f61373269192a63cf263
BhavanaEE <bhavana.chivukula@everest.engineering>
2021-12-10 12:37:42 +0530

    Updated flight service by reading and writing data from folder

--- src/main/java/com/everest/service/SearchFlightService.java
+++ src/main/java/com/everest/service/SearchFlightService.java
@@ -14,60 +14,73 @@ import java.util.stream.Stream;
 
 @Component
 public class SearchFlightService {
-    public List<Flight> searchFlightsBySourceAndDestination(String source, String destination) throws IOException {
+    File folder = new File("/Users/bhavanachivukula/Training/airlines/src/main/java/com/everest/database/FlightsData");
+    public List<Flight> readFromFolder(File folder, String source, String destination) throws IOException {
         List<Flight> flights = new ArrayList<>();
-        Stream<String> lines = Files.lines(Paths.get("/Users/bhavanachivukula/Training/airlines/src/main/java/com/everest/airline/database/Data.txt"));
-        flights=lines
-                .filter(line->{
-                    String[] split = line.split(",");
-                    return split[1].equalsIgnoreCase(source) && split[2].equalsIgnoreCase(destination);
-                })
-                .map(line->{
-                    String[] flightDetails = line.split(",");
-                    return new Flight(Long.parseLong(flightDetails[0]), flightDetails[1], flightDetails[2], LocalDate.parse(flightDetails[3]), LocalTime.parse(flightDetails[4]), LocalTime.parse(flightDetails[5]),Integer.parseInt(flightDetails[6]));
-                })
-                .collect(Collectors.toList());
-        lines.close();
+        for (File fileEntry : folder.listFiles()) {
+            if (fileEntry.isDirectory()) {
+                readFromFolder(fileEntry, source, destination);
+            } else {
+                Stream<String> lines = Files.lines(Paths.get(fileEntry.getPath()));
+                lines
+                        .filter(line -> {
+                            String[] split = line.split(",");
+                            return split[1].equalsIgnoreCase(source) && split[2].equalsIgnoreCase(destination);
+                        })
+                        .map(line -> {
+                            String[] flightDetails = line.split(",");
+                            flights.add(new Flight(Long.parseLong(flightDetails[0]), flightDetails[1], flightDetails[2], LocalDate.parse(flightDetails[3]), LocalTime.parse(flightDetails[4]), LocalTime.parse(flightDetails[5]), Integer.parseInt(flightDetails[6])));
+                            return line;
+                        })
+                        .collect(Collectors.toList());
+            }
+        }
         return flights;
     }
 
-    public List<Flight> getFlights(String source, String destination,String departureDate) throws IOException {
-        List<Flight> flightList=searchFlightsBySourceAndDestination(source,destination);
-        flightList = getFlightsByDepartureDate(flightList,departureDate);
+    public List<Flight> searchFlightsBySourceAndDestination(String source, String destination) throws Exception {
+        List<Flight> flights = new ArrayList<>();
+        flights = readFromFolder(folder, source, destination);
+        return flights;
+    }
+
+    public List<Flight> getFlights(String source, String destination, String departureDate) throws Exception {
+        List<Flight> flightList = searchFlightsBySourceAndDestination(source, destination);
+        flightList.forEach(System.out::println);
+        flightList = getFlightsByDepartureDate(flightList, departureDate);
         return getFlightsBySeatsAvailable(flightList);
 
     }
 
-    public List<Flight> getFlightsByDepartureDate(List<Flight> flightList, String departureDate){
+    public List<Flight> getFlightsByDepartureDate(List<Flight> flightList, String departureDate) {
         List<Flight> flights = flightList.stream().filter(flight -> flight.getDepartureDate().equals(LocalDate.parse(departureDate))).collect(Collectors.toList());
         return flights;
     }
-    public List<Flight> getFlightsBySeatsAvailable(List<Flight> flightList){
-        return flightList.stream().filter(flight -> flight.getSeatsAvailable()>1).collect(Collectors.toList());
+
+    public List<Flight> getFlightsBySeatsAvailable(List<Flight> flightList) {
+        return flightList.stream().filter(flight -> flight.getSeatsAvailable() >1).collect(Collectors.toList());
     }
+
     public void updateFlightData(List<Flight> modifiedFlights) throws IOException {
-        for(int i=0;i<modifiedFlights.size();i++){
-            if(modifiedFlights.get(i).getSeatsAvailable()>0) {
-                writeToFile(modifiedFlights.get(i));
-                break;
-            }
-        }
+        int seatsAvailable = modifiedFlights.get(0).getSeatsAvailable();
+        modifiedFlights.get(0).setSeatsAvailable(seatsAvailable - 1);
+        writeToFile(modifiedFlights.get(0));
     }
+
     public void writeToFile(Flight modifiedFlights) throws IOException {
         List<String> flights = new ArrayList<>();
-        Stream<String> lines = Files.lines(Paths.get("/Users/bhavanachivukula/Training/airlines/src/main/java/com/everest/airline/database/Data.txt"));
-        flights=lines
-                .map(line->{
-                    String[] flightDetails = line.split(",");
-                    if(flightDetails[0].equals(String.valueOf(modifiedFlights.getNumber()))){
-                        int seatsAvailable=modifiedFlights.getSeatsAvailable();
-                        modifiedFlights.setSeatsAvailable(seatsAvailable-1);
-                        line=flightDetails[0]+","+flightDetails[1]+","+flightDetails[2]+","+flightDetails[3]+","+flightDetails[4]+","+flightDetails[5]+","+modifiedFlights.getSeatsAvailable();
-                    }
-                    return line;
-                })
-                .collect(Collectors.toList());
-        Files.write(Path.of("/Users/bhavanachivukula/Training/airlines/src/main/java/com/everest/airline/database/Data.txt"),flights);
-        lines.close();
+        for (File fileEntry : Objects.requireNonNull(folder.listFiles())) {
+            Stream<String> lines = Files.lines(Paths.get(fileEntry.getPath()));
+            flights=lines
+                    .map(line -> {
+                        String[] flightDetails = line.split(",");
+                        if (flightDetails[0].equals(String.valueOf(modifiedFlights.getNumber()))) {
+                            line = flightDetails[0] + "," + flightDetails[1] + "," + flightDetails[2] + "," + flightDetails[3] + "," + flightDetails[4] + "," + flightDetails[5] + "," + modifiedFlights.getSeatsAvailable();
+                        }
+                        return line;
+                    })
+                    .collect(Collectors.toList());
+            Files.write(Path.of(fileEntry.getPath()), flights);
+        }
     }
 }
