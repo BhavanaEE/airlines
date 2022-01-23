bfa087ee8bbdca5cc0837c79dc5bff1cea020812
BhavanaEE <bhavana.chivukula@everest.engineering>
2022-01-23 23:30:06 +0530

    Implemented CRUD operations using database.

--- src/main/java/com/everest/controller/FlightsApiController.java
+++ src/main/java/com/everest/controller/FlightsApiController.java
@@ -1,42 +1,56 @@
 package com.everest.controller;
 
-import com.everest.database.DataHandler;
-import com.everest.exception.FlightsNotFound;
+import com.everest.FlightRowMapper;
+import com.everest.dto.FlightDTO;
 import com.everest.model.Flight;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.web.bind.annotation.*;
 
-import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 public class FlightsApiController {
+    @Autowired
+    private FlightDTO flightDTO;
 
     @Autowired
-    private DataHandler dataHandler;
+    private NamedParameterJdbcTemplate jdbcTemplate;
 
     @GetMapping("/flights")
-    public List<Flight> getAllFlights() throws IOException {
-        return dataHandler.readDataFromFolder();
+    public List<Flight> getAllFlights(){
+        return jdbcTemplate.query("SELECT * FROM flight_database.Flight f join flight_database.FlightDetail fd  on f.`Number` = fd.`Number`", new FlightRowMapper());
     }
 
-    @GetMapping("/flights/{number}")
-    public Flight getFlight(@PathVariable("number") long number) throws IOException, FlightsNotFound {
-        return dataHandler.readFlight(number);
+    @GetMapping("/flight/{number}")
+    public Flight getFlight(@PathVariable("number") long number){
+        Map<String,Object> map=new HashMap<>();
+        map.put("number",number);
+        return jdbcTemplate.queryForObject("SELECT * FROM flight_database.Flight f join flight_database.FlightDetail fd  on f.`Number` =fd.`Number` and f.`Number` =:number", map, new FlightRowMapper());
     }
 
-    @PostMapping("/flights")
-    public long create(@RequestBody Flight flight) throws IOException {
-        return dataHandler.createFlight(flight);
+    @PostMapping("/flight")
+    public long create(@RequestBody Flight flight){
+        Map<String,Object> map=flightDTO.getFlightDetails(flight);
+        jdbcTemplate.update("insert into flight_database.Flight(Number,Source,Destination,DepartureDate,DepartureTime,ArrivalTime) values(:number,:source,:destination,:departureDate,:departureTime,:arrivalTime)",map);
+        jdbcTemplate.update("insert into flight_database.FlightDetail values(:totalEconomicSeats,:availableEconomicSeats,:economicBasePrice,:totalFirstClassSeats,:availableFirstClassSeats,:firstClassBasePrice,:totalSecondClassSeats,:availableSecondClassSeats,:secondClassBasePrice,:number)",map);
+        return flight.getNumber();
     }
 
-    @PutMapping("/flights/{number}")
-    public Flight update(@PathVariable long number,@RequestBody Flight flight) throws IOException, FlightsNotFound {
-        return dataHandler.updateFlight(number,flight);
+    @PutMapping("/flight/{number}")
+    public Flight update(@PathVariable long number,@RequestBody Flight flight){
+        Map<String,Object> map=flightDTO.getFlightDetails(flight);
+        jdbcTemplate.update("update flight_database.Flight f set f.Number=:number,f.Source =:source,f.Destination=:destination,f.DepartureDate=:departureDate,f.DepartureTime=:departureTime,f.ArrivalTime=:arrivalTime where f.Number=:number",map);
+        jdbcTemplate.update("UPDATE flight_database.FlightDetail fd set fd.TotalEconomicSeats=:totalEconomicSeats,fd.AvailableEconomicSeats=:availableEconomicSeats,fd.EconomicBasePrice=:economicBasePrice,fd.TotalFirstClassSeats=:totalFirstClassSeats,fd.AvailableFirstClassSeats=:availableFirstClassSeats,fd.FirstClassBasePrice=:firstClassBasePrice,fd.TotalSecondClassSeats=:totalSecondClassSeats,fd.AvailableSecondClassSeats=:availableSecondClassSeats,fd.SecondClassBasePrice=:secondClassBasePrice where fd.Number=:number",map);
+        return jdbcTemplate.queryForObject("SELECT * FROM flight_database.Flight f join flight_database.FlightDetail fd  on f.`Number` =fd.`Number` and f.`Number` =:number", map, new FlightRowMapper());
     }
 
-    @DeleteMapping("/flights/{number}")
-    public boolean delete(@PathVariable("number") long number) throws IOException, FlightsNotFound {
-        return dataHandler.deleteFlight(number);
+    @DeleteMapping("/flight/{number}")
+    public int delete(@PathVariable("number") long number){
+        Map<String ,Object> map=new HashMap<>();
+        map.put("Number",number);
+        return jdbcTemplate.update("delete from Flight where Number=:Number",map);
     }
 }
